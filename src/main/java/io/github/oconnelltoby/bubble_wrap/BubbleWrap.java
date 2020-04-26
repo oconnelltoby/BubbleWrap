d09a1519093a3114ab009ad1cac914dd5c15db29
Toby O'Connell <tobyoconnell@hotmail.com>
2020-04-26 22:28:22 +0100

    Replace Example mod stub with Bubble Wrap mod

---
+++ src/main/java/io/github/oconnelltoby/bubble_wrap/BubbleWrap.java
@@ -0,0 +1,8 @@
+package io.github.oconnelltoby.bubble_wrap;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BubbleWrap.MODID)
+public final class BubbleWrap {
+    public static final String MODID = "bubble_wrap";
+}
