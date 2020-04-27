5eb199cd4b22edb532ef35e58a787cb8b51a8d1c
Toby O'Connell <tobyoconnell@hotmail.com>
2020-04-27 09:14:38 +0100

    Make Bubble Wrap play a sound on right click

--- src/main/java/io/github/oconnelltoby/bubble_wrap/item/BubbleWrapItem.java
+++ src/main/java/io/github/oconnelltoby/bubble_wrap/item/BubbleWrapItem.java
@@ -1,7 +1,14 @@
 package io.github.oconnelltoby.bubble_wrap.item;
 
+import io.github.oconnelltoby.bubble_wrap.BubbleWrap;
 import io.github.oconnelltoby.bubble_wrap.init.BubbleWrapItemGroups.BubbleWrapItemGroup;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BubbleWrapItem extends Item {
     public BubbleWrapItem(Properties properties) {
@@ -12,5 +19,22 @@ public class BubbleWrapItem extends Item {
 
     public BubbleWrapItem() {
         super(defaultProperties);
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+
+        ResourceLocation location = new ResourceLocation(BubbleWrap.MODID, "bubble_wrap_pop");
+        SoundEvent popSoundEvent = new SoundEvent(location);
+        SoundCategory soundCategory = SoundCategory.PLAYERS;
+
+        Random random = new Random();
+
+        float volume = 0.8f + 0.2f * random.nextFloat(); // 0.8 to 1.0 volume
+        float pitch = 0.9f + 0.2f * random.nextFloat(); // 0.9 to 1.1 pitch
+
+        worldIn.playSound(playerIn, playerIn.getPosition(), popSoundEvent, soundCategory, volume, pitch);
+
+        return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 }
